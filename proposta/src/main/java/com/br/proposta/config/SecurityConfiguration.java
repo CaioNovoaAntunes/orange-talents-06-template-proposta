cf3915ea8af59fe16d8e0bbf207bde8cfc58356e
CaioNovoaAntunes <caio_novoa_antunes@mail.com>
2021-07-30 16:34:33 -0300

    login via senha cartao

---
+++ proposta/src/main/java/com/br/proposta/config/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.br.proposta.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests -> authorizeRequests
+                .antMatchers(HttpMethod.GET, "/proposta/**").hasAuthority("SCOPE_proposta:read")
+                .antMatchers(HttpMethod.POST, "/proposta/**").hasAuthority("SCOPE_proposta:write")
+                .antMatchers(HttpMethod.GET, "/actuator/**").hasAuthority("SCOPE_proposta:read")
+                .anyRequest().authenticated()).sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).csrf().disable()
+        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
